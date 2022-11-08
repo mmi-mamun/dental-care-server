c06761dad36f7446506f4f292f4c397f631a523b
mmi-mamun <muzahidulislam735@gmail.com>
2022-11-09 04:05:40 +0600

    Working of get data using params is done

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.listen(port, () => {
 
 //mongodb
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ijax3zt.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -33,6 +33,20 @@ async function run() {
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
+        })
+
+        // app.get('services/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     console.log(req);
+        //     const query = { _id: ObjectId(id) };
+        //     const service = await serviceCollection.findOne(query);
+        //     res.send(service);
+        // })
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
         })
 
 
